fff0fb961ab9e61fcb540afcb465c0bcf02eae0f
yuhl <huanlong115@163.com>
2015-03-16 11:35:24 +0800

    Test
    for test

---
+++ Test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#coding=utf-8
+import os
+import re
+import time
+import sys
+import signal
+import threading
+
+
+list = (1,2,3);
+length = len(list)
+print "==========%s==========",length
