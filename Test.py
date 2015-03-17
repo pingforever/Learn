ab93d511e7d2c072bec58762bd87d12331724eff
yuhl <huanlong115@163.com>
2015-03-17 10:35:59 +0800

    1
    1

--- Test.py
+++ Test.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 #coding=utf-8
+from multiprocessing import Process
 import os
-import re
-import time
-import sys
-import signal
-import threading
 
+# 子进程要执行的代码
+def run_proc(name):
+    print 'Run child process %s (%s)...' % (name, os.getpid())
 
-list = (1,2,3);
-length = len(list)
-print "==========%s==========",length
+if __name__=='__main__':
+    print 'Parent process %s.' % os.getpid()
+    p = Process(target=run_proc, args=('test',))
+    print 'Process will start.'
+    p.start()
+    p.join()
+    print 'Process end.'
